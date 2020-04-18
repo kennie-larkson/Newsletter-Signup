e4b28983f0ec68fdb32dfb0bd6970b03d93bfa3b
LAWAL ABD'KEHINDE <materialkenny@gmail.com>
2020-04-18 11:35:59 +0100

    server set up and received form data from browser, also created jsonData to be used in a post request to mailchimp API

--- app.js
+++ app.js
@@ -9,7 +9,48 @@ const port = process.env.PORT || 3000
 
 
 const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"))
+
+app.get('/',(req,res)=>{
+    res.sendFile(`${__dirname}/signup.html`)
+})
+
+app.post('/',(req,res)=>{
+    const subscriberFname = req.body.fname
+    const subscriberLname = req.body.lname
+    const subscriberEmail = req.body.email
+    console.log(`
+    ${subscriberFname}
+    ${subscriberLname}
+    ${subscriberEmail}
+    `)
+
+    const data = {
+        member:{
+            email_address: subscriberEmail,
+            status: "subscribed",
+            merge_fields:{
+                FNAME: subscriberFname,
+                LNAME: subscriberLname
+            }
+        }
+    }
+
+    const jsonData = JSON.stringify(data)
+})
+
+//{"name":"Freddie'\''s Favorite Hats","contact":{"company":"Mailchimp","address1":"675 Ponce De Leon Ave NE","address2":"Suite 5000","city":"Atlanta","state":"GA","zip":"30308","country":"US","phone":""},"permission_reminder":"You'\''re receiving this email because you signed up for updates about Freddie'\''s newest hats.","campaign_defaults":{"from_name":"Freddie","from_email":"freddie@freddiehats.com","subject":"","language":"en"},"email_type_option":true}
+
+
 
 app.listen(port, ()=>{
     console.log(`Newsletter server running on port: ${port}`)
 })
+
+
+//API KEY
+//468bfcb3e94d38d459b9f7ea5f0db5a0-us4
+
+//List ID
+//3f5533cdcd
